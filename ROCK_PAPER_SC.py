831a862a1a6bbe949a453826bc17a86ad2d2fb6a
anubhavprasad21 <anubhavprasad21@gmail.com>
2026-05-17 20:05:19 +0530

    CHANGE USER WON TO YOU WON

--- ROCK_PAPER_SC.py
+++ ROCK_PAPER_SC.py
@@ -6,15 +6,15 @@ def rps_game(inp):
     if user_choice==1 and computer_choice==2:
         print(f"Computer Won\nComputer={d1.get(computer_choice)}\nUser={d1.get(user_choice)}")
     elif user_choice==2 and computer_choice==1:
-        print(f"User Won\nComputer={d1.get(computer_choice)}\nUser={d1.get(user_choice)}")
+        print(f"You Won\nComputer={d1.get(computer_choice)}\nUser={d1.get(user_choice)}")
     elif user_choice==3 and computer_choice==1:
         print(f"Computer Won\nComputer={d1.get(computer_choice)}\nUser={d1.get(user_choice)}")
     elif user_choice==1 and computer_choice==3:
-        print(f"User Won\nComputer={d1.get(computer_choice)}\nUser={d1.get(user_choice)}")
+        print(f"You Won\nComputer={d1.get(computer_choice)}\nUser={d1.get(user_choice)}")
     elif user_choice==2 and computer_choice==2:
         print("Its a Draw\nComputer and You Both Choose Paper")
     elif user_choice==3 and computer_choice==2:
-        print(f"User Won\nComputer={d1.get(computer_choice)}\nUser={d1.get(user_choice)}")
+        print(f"You Won\nComputer={d1.get(computer_choice)}\nUser={d1.get(user_choice)}")
     elif user_choice==1 and computer_choice==1:
         print("Its A Draw\nComputer and You Both Choose Rock")
     elif user_choice==2 and computer_choice==3:
