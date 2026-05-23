e57a846e0655c1aa9b7917f753ecadc9642df4f7
anubhavprasad21 <anubhavprasad21@gmail.com>
2026-05-23 11:54:09 +0530

    New game added

---
+++ THE_PERFECT_GUESS.py
@@ -0,0 +1,15 @@
+import random
+cnum=random.randint(1,100)
+guess=1
+while cnum != -1:
+    n=int(input("Guess The Number:"))
+    if n>cnum:
+        print("Lower")
+        guess+=1
+    elif n<cnum:
+        print("Greater")
+        guess+=1
+    else :
+        break
+print("BINGO")
+print(f"The Number {cnum} you guessed in {guess} attempts")
